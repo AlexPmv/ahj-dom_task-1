3fa6dfcfb7f6a32f328afe5b59d06cf2c56328b1
Alex Palmov <likeitsweet@mail.ru>
2022-11-08 21:12:44 +0300

    margin added

--- docs/main.js
+++ docs/main.js
@@ -1,11 +1,1 @@
-(() => {
-  class e {
-    constructor() { this.itemCellArray = document.querySelectorAll('.item-cell'), this.createGoblinElement(), this.changeCell(); }
-
-    createGoblinElement() { const e = document.createElement('img'); e.classList.add('goblin-img'), e.src = './src/img/goblin.png', this.goblinElement = e; }
-
-    getRandomFlag() { const e = Math.floor(Math.random() * (this.itemCellArray.length - 1)); this.currentFlag = e === this.currentFlag ? this.getRandomFlag() : e; }
-
-    changeCell() { this.getRandomFlag(), this.itemCellArray[this.currentFlag].appendChild(this.goblinElement), setInterval((() => { this.getRandomFlag(), this.itemCellArray[this.currentFlag].appendChild(this.goblinElement); }), 2e3); }
-  }document.addEventListener('DOMContentLoaded', (() => { new e(); }));
-})();
+(()=>{"use strict";class e{constructor(){this.itemCellArray=document.querySelectorAll(".item-cell"),this.createGoblinElement(),this.changeCell()}createGoblinElement(){const e=document.createElement("img");e.classList.add("goblin-img"),e.src="./src/img/goblin.png",this.goblinElement=e}getRandomFlag(){const e=Math.floor(Math.random()*(this.itemCellArray.length-1));this.currentFlag=e===this.currentFlag?this.getRandomFlag():e}changeCell(){this.getRandomFlag(),this.itemCellArray[this.currentFlag].appendChild(this.goblinElement),setInterval((()=>{this.getRandomFlag(),this.itemCellArray[this.currentFlag].appendChild(this.goblinElement)}),2e3)}}document.addEventListener("DOMContentLoaded",(()=>{new e}))})();
